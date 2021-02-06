e9828ffd727d629ec0f45b112477a79f3afb6ca4
aelf-hz <mason.huang@aelf.io>
2021-02-06 13:26:28 +0800

    feat: 🎸 restart

--- app/common/constants.js
+++ app/common/constants.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const walletURL = 'http://tdvv-wallet-test.aelf.io';
-
 module.exports = {
   commonPrivateKey:
     '0000000000000000000000000000000000000000000000000000000000000001',
   // You can change the params for keyStore here
-  httpProvider: `${walletURL}/chain`,
+  httpProvider: 'http://172.31.47.183:8000',
 
   lotteryContract: 'buePNjhmHckfZn9D8GTL1wq6JgA8K24SeTWnjCNcrz6Sf1FDh',
 };
